8762e226e21e4b5d8b50d747ea99310bb616e47b
dreamsViolin <merve5sume@gmail.com>
2026-04-26 23:39:00 +0300

    Merve
    Kullanıcı sınıfına giriş yaptım

--- src/camasir/models/Kullanici.java
+++ src/camasir/models/Kullanici.java
@@ -1,5 +1,38 @@
 package camasir.models;
 
 public abstract class Kullanici {
+	private String TC;
+	private String telNo;
+	private String tamIsim;
+	public Kullanici(String tC, String telNo, String tamIsim) {
+		super();
+		TC = tC;
+		this.telNo = telNo;
+		this.tamIsim = tamIsim;
+	}
+	public String getTC() {
+		return TC;
+	}
+	public void setTC(String tC) {
+		if(tC != null &&tC.length()== 11 && tC.matches("\\d+")) {
+			TC = tC;
+		}else {
+			System.out.println("TC kimlik numarasi 11 karakter olmak zorundadir!");
+		}
+	}
+	public String getTelNo() {
+		return telNo;
+	}
+	public void setTelNo(String telNo) {
+		this.telNo = telNo;
+	}
+	public String getTamIsim() {
+		return tamIsim;
+	}
+	public void setTamIsim(String tamIsim) {
+		this.tamIsim = tamIsim;
+	}
+	
+
 
 }
