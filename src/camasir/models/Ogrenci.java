33c9c2543390675fb5f22a13f74751b5c3c7cc69
dreamsViolin <merve5sume@gmail.com>
2026-04-27 00:04:59 +0300

    merve
    constructo öğrenci için

--- src/camasir/models/Ogrenci.java
+++ src/camasir/models/Ogrenci.java
@@ -2,4 +2,10 @@ package camasir.models;
 
 public class Ogrenci extends Kullanici{
 
+	public Ogrenci(String tC, String telNo, String tamIsim) {
+		super(tC, telNo, tamIsim);
+		// TODO Auto-generated constructor stub
+	}
+	
+
 }
