745396596c4bca79685442e8d3b822f4a00ede22
dreamsViolin <merve5sume@gmail.com>
2026-05-07 00:38:59 +0300

    Giriş Arayüzü butonlandı

--- src/camasir/ui/GirisEkrani.java
+++ src/camasir/ui/GirisEkrani.java
@@ -3,10 +3,26 @@ import javax.swing.*;
 
 public class GirisEkrani extends JFrame {
 	public GirisEkrani() {
-		 this.setTitle("Camasir Randevu Sistemi");
-		 this.setSize(1000,600);
-		 setLocationRelativeTo(null);
+		 this.setTitle("Camasir Randevu Sistemi");   
+		 this.setSize(1000,600);                //pencerenin boyutları için
+		 setLocationRelativeTo(null);         //pencerenin ekranın tam ortasında olması için
 		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 this.setLayout(null);
+		 JTextField[] txtGiris = new JTextField[2];
+		 txtGiris[0] = new JTextField();
+		 txtGiris[1] = new JTextField();
+		 txtGiris[0].setBounds(250,100, 450,50);
+		 txtGiris[1].setBounds(250,150, 450,50);
+		 this.add(txtGiris[0]);
+		 this.add(txtGiris[1]);
+		 JButton btnGiris = new JButton("Giris");
+		 btnGiris.setBounds(400,220, 120,45);
+		 this.add(btnGiris);
+		 JLabel lblGiris = new JLabel("İsminizi ve Tc kimlik numaranızı giriniz:");
+		 lblGiris.setBounds(300,50,700,50);
+		 this.add(lblGiris);
+		 
+		 
 		 
 
 	}
--- src/camasir/ui/MAIN_CALISTIRICI.java
+++ src/camasir/ui/MAIN_CALISTIRICI.java
@@ -1,11 +1,21 @@
 package camasir.ui;
 
+import java.awt.EventQueue;
+
 public class MAIN_CALISTIRICI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GirisEkrani ilkEkran = new GirisEkrani();
-		ilkEkran.setVisible(true);
+		EventQueue.invokeLater(new Runnable() {
+	        public void run() {
+	            try {
+	            	GirisEkrani ilkEkran = new GirisEkrani();
+	            	ilkEkran.setVisible(true);
+	            } catch (Exception e) {
+	                e.printStackTrace(); // Hata çıkarsa konsola kırmızı yazıyla fırlat
+	            }
+	        }
+	    });
 
 	}
 
