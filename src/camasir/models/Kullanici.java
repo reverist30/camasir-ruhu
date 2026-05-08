328301f6ed496e595643fd9874a7795ddbe1f8f9
dreamsViolin <merve5sume@gmail.com>
2026-05-09 00:49:22 +0300

    Giriş ekranı bitirildi

--- src/camasir/models/Kullanici.java
+++ src/camasir/models/Kullanici.java
@@ -13,11 +13,11 @@ public abstract class Kullanici {
 	public String getTC() {
 		return TC;
 	}
-	public void setTC(String tC) {
+	public void setTC (String tC) throws Exception {
 		if(tC != null && tC.length() == 11 && tC.matches("\\d+")) {     //boş mu 11 tane mi ve rakam mı?
 			TC = tC;
 		} else {
-			System.out.println("TC kimlik numarasi 11 karakter olmak zorundadir!");
+			throw new Exception("TC Kimlik numarası tam 11 hane olmak zorundadır!");
 		}
 	}
 	
--- src/camasir/ui/GirisEkrani.java
+++ src/camasir/ui/GirisEkrani.java
@@ -20,7 +20,7 @@ public class GirisEkrani extends JFrame {
 		 setLocationRelativeTo(null);         //pencerenin ekranin tam ortasinda olmasi icin
 		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 this.setLayout(null);
-		 this.setBackground(Color.BLUE);
+		 this.getContentPane().setBackground(new Color(200, 220, 255));
 		 
 		 JTextField[] txtGiris = new JTextField[2];
 		 txtGiris[0] = new JTextField();
@@ -34,6 +34,8 @@ public class GirisEkrani extends JFrame {
 		 btnGiris.setForeground(Color.BLACK);
 		 btnGiris.setFont(new Font("Arial", Font.BOLD, 14));
 		 btnGiris.setBounds(420,250, 120,45);
+		 btnGiris.setOpaque(true);
+		 btnGiris.setBorderPainted(false);
 		 btnGiris.setBackground(new Color(100, 200, 120));
 		 btnGiris.setFocusPainted(false);
 		 this.add(btnGiris);
@@ -42,27 +44,70 @@ public class GirisEkrani extends JFrame {
 		 lblGiris[0]= new JLabel("İsminizi yazınız");
 		 lblGiris[1]= new JLabel("TC kimlik numaranızı yazınız");
 		 lblGiris[2]= new JLabel("");
-		 lblGiris[2].setForeground(Color.RED);        //yazının rengi 
+		 lblGiris[0].setBounds(250,80,700,20); 
+		 lblGiris[1].setBounds(250,150,700,20);
+		 lblGiris[2].setBounds(290,330,700,50);
+		 for(int i=0;i<lblGiris.length;i++) {
+			 if(i==0||i==1) {
+				 lblGiris[i].setFont(new Font("Arial", Font.PLAIN, 14));				 
+			 }else {
+				 lblGiris[i].setFont(new Font("Tahoma", Font.BOLD, 24));
+				 lblGiris[i].setForeground(Color.RED);
+			 }
+			 this.add(lblGiris[i]);
+		 }
+		 
+		 
+		 
+		 
+	/*	 lblGiris[2].setForeground(Color.RED);        //yazının rengi 
+		 lblGiris[3].setForeground(Color.RED);
 		 lblGiris[0].setFont(new Font("Arial", Font.PLAIN, 14));
 		 lblGiris[1].setFont(new Font("Arial", Font.PLAIN, 14));
 		 lblGiris[0].setBounds(250,80,700,20); 
 		 lblGiris[1].setBounds(250,150,700,20);
 		 lblGiris[2].setBounds(380,330,700,20);
+		 lblGiris[3].setBounds(380,330,700,20);
 		 this.add(lblGiris[0]); 
 		 this.add(lblGiris[1]);
 		 this.add(lblGiris[2]);
+		 this.add(lblGiris[3]);
+	*/	 
 		 btnGiris.addActionListener(e -> {
-			 String ogrTC = txtGiris[1].getText();
-			 if(ogrTC.equals("11111111111")) {
-					MainDashboard frame = new MainDashboard();
-					frame.setVisible(true);
-					this.dispose();		 
+			 String ogrIsim = txtGiris[0].getText().trim();
+			 String ogrTC = txtGiris[1].getText().trim();
+			 if(ogrIsim.isEmpty() || ogrTC.isEmpty()) {
+				 lblGiris[2].setText("Hatalı Giriş! Boşlukları doldurunuz.");
+				 return;
+			 
+			 }if(ogrTC.length()!=11) {
+				 lblGiris[2].setText("Hatalı Giriş! TC 11 haneli olmalı!");
+				 return ;
+			 }else if(ogrTC.equals("11111111111")) {
+				   if(!ogrIsim.equalsIgnoreCase("Merve")) {
+					 lblGiris[2].setText("Hatalı Giriş! TC ile isim uyuşmamaktadır.");	
+					 return ;
+			     }else {
+						MainDashboard frame = new MainDashboard();
+						frame.setVisible(true);
+						this.dispose();	
+						return ;
+			     }
+	 
 			 }else if(ogrTC.equals("22222222222")) {
-					CamasirApp frame = new CamasirApp();
-					frame.setVisible(true);
-					this.dispose();
+				 if(!ogrIsim.equalsIgnoreCase("Belkıs")) {
+					 lblGiris[2].setText("Hatalı Giriş! TC ile isim uyuşmamaktadır.");		
+					 return ;
+				 }else {
+						CamasirApp frame = new CamasirApp();
+						frame.setVisible(true);
+						this.dispose();
+						return ;
+				 }
+
 			 }else {
-				 lblGiris[2].setText("Hatalı Giriş! Bu TC sistemde kayıtlı değil.");
+				 lblGiris[2].setText("Hatalı Giriş! Önce kayıt olunuz.");
+				 return ;
 			 }
 			});
 		 
